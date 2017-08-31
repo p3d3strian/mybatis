ef3f7b4f115a27263e4135c1a2b9fc4a445a421d
rmsdfg <rmsdfg@KAMANI>
2017-08-31 23:01:54 +0800

    添加了log4f2.xml文件

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,7 +1,9 @@
 package dao;
 
+import java.util.List;
+
 import entity.User;
 
 public interface UserDao {
-	User findUser();
+	List<User> findUser();
 }
--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -12,14 +13,13 @@ import dao.UserDao;
 import entity.User;
 
 public class App {
-	SqlSession ss;
+	SqlSessionFactory ssf;
 	public App(){
 		try {
 			 Reader reader = Resources.getResourceAsReader("config/config.xml");
 			
-			 SqlSessionFactory ssf = new SqlSessionFactoryBuilder().build(reader);
+			 ssf = new SqlSessionFactoryBuilder().build(reader);
 			 
-			 ss=ssf.openSession();
 		} catch (IOException e) {
 			System.err.println("读取资源文件出错啦   "+e);
 		}
@@ -32,9 +32,18 @@ public class App {
     }
 
 	private void findUser() {
-		UserDao Usermapper = ss.getMapper(UserDao.class);
-		User findUser = Usermapper.findUser();
-		System.out.println(findUser);
+		SqlSession ss1=ssf.openSession();
+		UserDao mapper1 = ss1.getMapper(UserDao.class);
+		mapper1.findUser();
+//		ss.commit();
+//		mapper1.findUser();
+		ss1.close();
+		
+		//开启一个新的session，由于每个session不共享一级缓存，所以该session没有缓存
+		SqlSession ss2=ssf.openSession();
+		UserDao mapper2 = ss2.getMapper(UserDao.class);
+		mapper2.findUser();
+		
 	}
     
 }
